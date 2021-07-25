d1de46c5acb5c3ec1f11ef469b7848490f17fefe
Gerdivaldo <gerdsi2013@gmail.com>
2021-07-25 11:56:01 -0300

    funcionalidade do locastore

--- src/pages/CadastroPessoal/index.jsx
+++ src/pages/CadastroPessoal/index.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../CadastroPessoal/stylo/cadastro.scss";
+import "../CadastroProduto/stylo/produto.scss";
 
 export function CadastroPessoal() {
-  return (
+  const [nome, setnome] = useState("");
+  const [cpf, setcpf] = useState("");
+  const [data, setdata] = useState("");
+  const [telefone, settelefone] = useState("");
+  const [email, setemail] = useState("");
+  const [endereco, setendereco] = useState("");
+  const [numero, setnumero] = useState("");
+  const [cidade, setcidade] = useState("");
+  const [bairro, setbairro] = useState("");
+  const [uf, setuf] = useState("");
+  const [cep, setcep] = useState("");
 
+  const handlebtn = () => {
+    localStorage.setItem("Nome:", nome);
+    localStorage.setItem("Cpf:", cpf);
+    localStorage.setItem("Data:", data);
+    localStorage.setItem("Telefone:", telefone);
+    localStorage.setItem("Email:", email);
+    localStorage.setItem("Endereço:", endereco);
+    localStorage.setItem("Numero:", numero);
+    localStorage.setItem("Cidade:", cidade);
+    localStorage.setItem("Bairro:", bairro);
+    localStorage.setItem("Uf:", uf);
+    localStorage.setItem("Cep:", cep);
+  };
+
+  return (
     <main className="container">
       <h2>Cadastro de clientes</h2>
 
@@ -14,26 +40,56 @@ export function CadastroPessoal() {
           <form className="formulario" action="">
             <div className="form-nome">
               <label>Nome Completo</label>
-              <input type="text" name="nome" id="nome" />
+              <input
+                type="text"
+                name="nome"
+                id="nome"
+                value={nome}
+                onChange={(e) => setnome(e.target.value)}
+              />
             </div>
 
             <div className="form-cpf-email">
-              <div className="cpf">
+              <div>
                 <label>Cpf:</label>
-                <input type="number" name="cpf" id="cpf" />
+                <input
+                  type="number"
+                  name="cpf"
+                  id="cpf"
+                  value={cpf}
+                  onChange={(e) => setcpf(e.target.value)}
+                />
               </div>
-              <div className="data">
+              <div>
                 <label>Data de Nascimento</label>
-                <input type="date" name="data" id="data" />
+                <input
+                  type="date"
+                  name="data"
+                  id="data"
+                  value={data}
+                  onChange={(e) => setdata(e.target.value)}
+                />
               </div>
-              <div className="contato">
+              <div>
                 <label>Contato</label>
-                <input type="text" name="telefone" id="telefone" />
+                <input
+                  type="text"
+                  name="telefone"
+                  id="telefone"
+                  value={telefone}
+                  onChange={(e) => settelefone(e.target.value)}
+                />
               </div>
 
               <div className="email">
                 <label>E-mail</label>
-                <input type="email" name="email" id="email" />
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setemail(e.target.value)}
+                />
               </div>
             </div>
           </form>
@@ -43,52 +99,92 @@ export function CadastroPessoal() {
           <h3>Informações adicionas</h3>
 
           <form className="formulario" action="">
-            
             <div className="endereco-numero">
-              <div className="endereco">
+              <div>
                 <label>Logradouro:</label>
-                <input type="text" name="endereco" id="endereco" />
+                <input
+                  type="text"
+                  name="endereco"
+                  id="endereco"
+                  value={endereco}
+                  onChange={(e) => setendereco(e.target.value)}
+                />
               </div>
 
-              <div className="numero">
+              <div>
                 <label>Numero:</label>
-                <input type="text" name="numero" id="numero" />
+                <input
+                  type="text"
+                  name="numero"
+                  id="numero"
+                  value={numero}
+                  onChange={(e) => setnumero(e.target.value)}
+                />
               </div>
             </div>
 
             <div className="cidade-bairo-uf">
-              <div className="cidade">
+              <div>
                 <label>Cidade</label>
-                <input type="text" name="cidade" id="cidade" />
+                <input
+                  type="text"
+                  name="cidade"
+                  id="cidade"
+                  value={cidade}
+                  onChange={(e) => setcidade(e.target.value)}
+                />
               </div>
 
-              <div className="bairro">
+              <div>
                 <label>Bairro</label>
-                <input type="text" name="bairro" id="bairro" />
+                <input
+                  type="text"
+                  name="bairro"
+                  id="bairro"
+                  value={bairro}
+                  onChange={(e) => setbairro(e.target.value)}
+                />
               </div>
               <div className="uf">
-                <label>UF</label>
-                <input type="text" name="uf" id="uf" />
+                <label>Uf</label>
+                <input
+                  type="text"
+                  name="uf"
+                  id="uf"
+                  value={uf}
+                  onChange={(e) => setuf(e.target.value)}
+                />
               </div>
             </div>
 
             <div className="comple-cep">
-              <div className="complemento">
+              <div>
                 <label>Complemento</label>
-                <input type="text" name="cidade" id="cidade" />
+                <input
+                  type="text"
+                  name="cidade"
+                  id="cidade"
+                  value={cidade}
+                  onChange={(e) => setcidade(e.target.value)}
+                />
               </div>
-              <div className="cep">
+              <div>
                 <label>Cep</label>
-                <input type="number" name="cep" id="cep" />
+                <input
+                  type="number"
+                  name="cep"
+                  id="cep"
+                  value={cep}
+                  onChange={(e) => setcep(e.target.value)}
+                />
               </div>
             </div>
+            <button className="btn" onClick={handlebtn}>
+              Enviar
+            </button>
           </form>
         </section>
       </div>
-
-       
-       
     </main>
-    
   );
 }
--- src/pages/CadastroProduto/index.jsx
+++ src/pages/CadastroProduto/index.jsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../CadastroProduto/stylo/produto.scss";
 
 export function CadastroProduto() {
+
+  const [descricao, setdescricao] = useState("");
+  const [cod, setcod] = useState("")
+  const [fabricante, setfabricante] = useState("")
+  const [fornecedor, setfornecedor] = useState("")
+  const [preco, setpreco] = useState("")
+  const [qtd, setqtd] = useState("")
+
+
+  const Handle = () =>{
+    localStorage.setItem("Descricao", descricao);
+    localStorage.setItem("Codigo",cod);
+    localStorage.setItem("Fabricante",fabricante);
+    localStorage.setItem("Forncedor",fornecedor);
+    localStorage.setItem("Preco",preco);
+    localStorage.setItem("Quantidade",qtd);
+  }
+
+
   return (
+
     <main className="container-produto">
       <h2>cadastro de produtos</h2>
 
       <form className="form" action="">
 
-
-
        
       <div className="descri-cod">
           <div className="descricao">
             <label>descrição</label>
-            <input type="text" name="descricao" id="descricao" />
+            <input 
+            type="text" 
+            name="descricao" 
+            id="descricao"
+            value={descricao}
+            onChange = {(e) => setdescricao(e.target.value)} />
           </div>
 
           <div className="codigo">
             <label>codigo</label>
-            <input type="text" name="codigo" id="codigo"/>
+            <input
+             type="text"
+              name="codigo"
+               id="codigo"
+               value={cod}
+               onChange = {(e)=>setcod(e.target.value)}/>
           </div>
         </div>
 
@@ -30,29 +58,45 @@ export function CadastroProduto() {
         <div className="fabr-fornec">
           <div className="fabricante">
             <label>fabricante</label>
-            <input type="text" name="fabricante" id="fabricante" />
+            <input type="text"
+             name="fabricante"
+              id="fabricante"
+              value = {fabricante}
+              onChange = {(e)=> setfabricante (e.target.value)} />
           </div>
 
           <div className="fornecedor">
             <label>fornecedor</label>
-            <input type="text" name="fornecedor" id="fornecedor" />
+            <input type="text"
+             name="fornecedor"
+              id="fornecedor"
+              value ={fornecedor}
+              onChange = {(e)=> setfornecedor (e.target.value)}/>
           </div>
         </div>
 
 <div className="preco-qtd">
         <div className="preco">
           <label>preço produto</label>
-          <input type="text" name="preco" id="preco" />
+          <input type="text"
+           name="preco"
+            id="preco"
+            value={preco}
+            onChange = {(e)=> setpreco(e.target.value)}/>
         </div>
 
         <div className="quantitade">
           <label>quantidade</label>
-          <input type="text" name="quantidade" id="quantidade" />
+          <input type="text"
+           name="quantidade"
+            id="quantidade"
+            value={qtd}
+            onChange = {(e)=> setqtd(e.target.value)}/>
         </div>
         </div>
-        
+        <button className="btn" onClick={Handle} >Enviar</button>
       </form>
-      <button className="btn" >Enviar</button>
+      
      
     </main>
   );
